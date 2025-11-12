4ac7e62d9f962cb8411d19f52ab3bbfaa3425b12
Xiaoxiao Li <xiaoxiao.li@sheffield.ac.uk>
2025-11-12 16:16:34 +0000

    🧹remove unnecessary functions

--- code/lib/sim.jl
+++ code/lib/sim.jl
@@ -8,78 +8,8 @@ function rescale_bodymass(bodymasses::AbstractVector, met_class::AbstractVector)
     return bodymasses
 end
 
-function get_spe_generality(A)
-    A = sparse(A)
-    S = size(A, 1)
-    generality = zeros(S)
-    for i in 1:S
-      preys = ENDI.preys_of(i, A)
-      length_preys = length(preys)
-      generality[i] = length_preys
-    end
-    return generality
-end
 
-function get_spe_vulnerability(A)
-    A = sparse(A)
-    S = size(A, 1)
-    vulnerability = zeros(S)
-    for i in 1:S
-      predators = ENDI.predators_of(i, A)
-      length_predators = length(predators)
-      vulnerability[i] = length_predators
-    end
-    return vulnerability
-end
-
-function get_fw_generality(A::AbstractMatrix)
-    g = get_spe_generality(A)
-    nz = g[g .> 0]
-    if isempty(nz)
-        return NaN
-    end
-    mean_generality = sum(g) / length(nz)   # same as R: sum(colSums)/count(colSum>0)
-    sd_generality   = length(nz) > 1 ? std(nz) : 0.0
-    return mean_generality, sd_generality
-end
-
-function get_fw_vulnerability(A::AbstractMatrix)
-    g = get_spe_vulnerability(A)
-    nz = g[g .> 0]
-    if isempty(nz)
-        return NaN
-    end
-    mean_vulnerability = sum(g) / length(nz)   # same as R: sum(colSums)/count(colSum>0)
-    sd_vulnerability   = length(nz) > 1 ? std(nz) : 0.0
-    return mean_vulnerability, sd_vulnerability
-end
-
-function get_IS_summary(params, Beq, alive_idx)
-    j = jacobian(params, Beq)
-    j_alive = j[alive_idx, alive_idx]
-    j_alive_abs = abs.(j_alive)
-    # all top-down interaction strengths
-    mean_abs_IS_all = mean(j_alive_abs[j_alive_abs .> 0])
-    sd_abs_IS_all = std(j_alive_abs[j_alive_abs .> 0])
-    skew_abs_IS_all = skewness(j_alive_abs[j_alive_abs .> 0])  
-    top_down_IS = tril(j_alive_abs, -1)
-    mean_top_down = mean(top_down_IS[top_down_IS .> 0])
-    sd_top_down = std(top_down_IS[top_down_IS .> 0])
-    bottom_up_IS = triu(j_alive_abs, 1)
-    mean_bottom_up = mean(bottom_up_IS[bottom_up_IS .> 0])
-    sd_bottom_up = std(bottom_up_IS[bottom_up_IS .> 0])
-    IS_summary = (
-         mean_top_down = mean_top_down,
-         mean_bottom_up = mean_bottom_up,
-         mean_abs_IS_all = mean_abs_IS_all,
-         sd_top_down = sd_top_down,
-         sd_bottom_up = sd_bottom_up,
-         sd_abs_IS_all = sd_abs_IS_all,
-         skew_abs_IS_all = skew_abs_IS_all
-      )
-    return IS_summary
-end
-
+# 
 function get_fw_resilience(params, Beq, alive_idx)
     j = jacobian(params, Beq)
     j_alive = j[alive_idx, alive_idx]
