dd9eef25ea976bee71a7830c3ee13711cd80a292
Tanya Strydom <tanya.strydom@icloud.com>
2026-03-30 12:16:51 +0100

    🐣 get END topology

---
+++ code/04_ENDTopology.jl
@@ -0,0 +1,65 @@
+#=
+-------------------------------------------------
+02_Topology.jl
+Calculating structure/topology for generated networks.
+-------------------------------------------------
+
+=#
+# Load the Pkg package manager.
+using Pkg
+# Activate the current project environment to ensure consistent package versions.
+Pkg.activate(".")
+
+# --- 1. Load Dependencies ---
+using CSV
+using DataFrames
+using JLD2
+using LinearAlgebra
+using Plots
+using SpeciesInteractionNetworks
+
+# --- 2. Load All Code ---
+include("lib/internals.jl");
+
+# --- 3. Import networks .jld2 object ---
+
+networks = load_object("data/outputs/END_final_adj.jld2")
+
+# --- 4. Convert networks to `SpeciesInteractionNetworks` networks ---
+
+# we will also append this to the network object as a col.
+networks.InteractionNetwork = build_network.(networks.adj_matrix)
+
+# --- 5. Get topology ---
+
+# create df to store outputs
+
+topology = DataFrame(
+    model = String[],
+    richness = Int64[],
+    connectance = Float64[],
+    complexity = Float64[],
+    trophic_level = Float64[],
+    generality = Float64[],
+    vulnerability = Float64[],
+    S1 = Float64[],
+    S2 = Float64[],
+    S4 = Float64[],
+    S5 = Float64[],
+);
+
+# note for simplicity we use a wrapper function
+
+for i in 1:nrow(networks)
+
+    d = network_summary(networks.InteractionNetwork[i])
+
+    d[:model] = networks.fw_ID[i]
+
+    push!(topology, d)
+
+end
+
+# write summaries as .csv
+CSV.write("data/outputs/END_topology.csv", topology)
+
