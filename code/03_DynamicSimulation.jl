94ed746a655c6a9d4a2e5600bb3b7e0e0aecdcf9
Xiaoxiao Li <xiaoxiao.li@sheffield.ac.uk>
2026-06-19 16:02:17 +0100

    update output metrics, clean file names, improve code format

--- code/03_DynamicSimulation.jl
+++ code/03_DynamicSimulation.jl
@@ -1,18 +1,22 @@
 #=
-Dynamic simulation output include:
+-------------------------------------------------
+03_Dynamicsimulation.jl
+-------------------------------------------------
 
-CSV summary output:
-- Persistence; 
-- Maximum trophic level; 
-- Total Biomass; 
-- CV of total biomass;  
-- Shannon; 
-- Evenness; 
-- Resilience; (using EcoNetPostProcessing.jl) 
-- Reactivity; (using EcoNetPostProcessing.jl) 
+This script performs dynamic simulations of generated food webs 
+and generates two output files.
 
-JLD2 network output:
-- Alive and connected adjacency matrix at equilibrium
+First, a jld2 file (post_networks.jld2) containing the post-simulation food web networks, 
+       represented as adjacency matrices of feeding links among species that remain alive and connected at equilibrium.
+
+Second, a CSV file (dynamic_metrics.csv) containing dynamic stability-related metrics for each food web:
+        - Persistence; 
+        - Mean realised trophic transfer efficiency; 
+        - Gini coefficient of energy fluxes;
+        - Skewness of absolute interaction strengths;
+        - Resilience;
+        - Reactivity; 
+
 =#
 
 using Pkg
@@ -25,6 +29,7 @@ using JLD2
 using EcologicalNetworksDynamics
 using EcoNetPostProcessing
 using DifferentialEquations
+using DiffEqCallbacks
 using Statistics
 
 # --- 2. Load All Code ---
@@ -32,45 +37,41 @@ include("lib/sim.jl");
 
 # --- 3. Import networks .jld2 object ---
 path = joinpath(@__DIR__, "data", "outputs", "network_test_verified_seed_42_29-03-2026.jld2")
-networks = load_object(path)
-sort!(networks, :fw_ID) 
+pre_networks = load_object(path)
+sort!(pre_networks, :fw_ID) 
 
 # --- 4. Run Dynamic Simulations ---
 tmin, tmax, tspan = 2000, 5000, 500
 
-simulation_summary = DataFrame(
+dynamic_metrics = DataFrame(
     fw_ID = String[],
     model = String[],
-    richness_equilibrium = Int[],
-    connectance_equilibrium = Float64[],
-    persistence = Float64[],
-    max_trophic_level = Float64[],
-    total_biomass = Float64[],
-    cv_total_biomass = Float64[],
-    shannon = Float64[],
-    evenness = Float64[],
-    resilience = Float64[],
-    reactivity = Float64[]
+    persistence = Float64[], # Persistence
+    mean_TTE = Float64[],    # Mean realised trophic transfer efficiency
+    gini_fluxes = Float64[], # Gini coefficient of energy fluxes
+    skewness_IS = Float64[], # Skewness of absolute interaction strengths
+    resilience = Float64[],  # Resilience
+    reactivity = Float64[]  # Reactivity
 )
 
-final_network = DataFrame(
+post_networks = DataFrame(
     fw_ID = String[],
     model = String[],
-    alive_connected_A = Any[]
+    post_adj = Any[]
 )
 
-for i in 1:nrow(networks)
-    @info "Simulating food web $(i) / $(nrow(networks))"
-    fwid         = networks.fw_ID[i]
-    model_name   = networks.Model[i]                
-    bodymasses   = networks.BodyMasses[i]
-    met_class    = networks.MetabolicClasses[i]
-    adj          = networks.AdjacencyMatrix[i]
+for i in 1:nrow(pre_networks)
+    @info "Simulating food web $(i) / $(nrow(pre_networks))"
+    fwid         = pre_networks.fw_ID[i]
+    model_name   = pre_networks.Model[i]                
+    body_masses  = pre_networks.BodyMasses[i]
+    met_class    = pre_networks.MetabolicClasses[i]
+    pre_adj      = pre_networks.AdjacencyMatrix[i]
 
-    fw = Foodweb(adj)  
+    fw = Foodweb(pre_adj)  
 
     if model_name in ("ADBM", "ATN", "LTM")
-        bodymasses_rescaled = rescale_bodymass(bodymasses, met_class)
+        bodymasses_rescaled = rescale_bodymass(body_masses, met_class)
         params = default_model(
             fw,
             BodyMass(bodymasses_rescaled),
@@ -89,7 +90,7 @@ for i in 1:nrow(networks)
     sol = simulate(
         params, B0, tmax;
         callback = CallbackSet(
-            extinction_callback(params, 1e-6; verbose = false),  # extinction threshold
+            extinction_callback(params, 1e-6; verbose = false),  # extinction threshold 1e-6
             TerminateSteadyState(1e-8, 1e-6, DiffEqCallbacks.allDerivPass; min_t = tmin),
         ),
         show_degenerated = false,
@@ -100,33 +101,35 @@ for i in 1:nrow(networks)
     catch err
         @warn "Simulation failed for fw_ID=$fwid" err
         (
-            richness_equilibrium = NaN,
-            connectance_equilibrium = NaN,
             persistence = NaN,
-            max_trophic_level = NaN,
-            total_biomass = NaN,
-            cv_total_biomass = NaN,
-            shannon = NaN,
-            evenness = NaN,
+            mean_TTE = NaN,
+            gini_fluxes = NaN,
+            skewness_IS = NaN,
             resilience = NaN,
             reactivity = NaN,
-            alive_connected_A = missing
+            post_adj = missing
         )
     end
 
     # Remove matrix object before saving summary to CSV
     summary_out = NamedTuple{
-        filter(k -> k != :alive_connected_A, keys(out))
+        filter(k -> k != :post_adj, keys(out))
     }(out)
 
-    push!(simulation_summary, (; fw_ID=string(fwid), model=string(model_name), summary_out...); promote = true)
+    # Save scalar dynamic metrics.
+    push!(dynamic_metrics, (; fw_ID=string(fwid), model = string(model_name), summary_out...); 
+                            promote = true)
 
-    push!(final_network, (fw_ID=string(fwid), model=string(model_name), alive_connected_A=out.alive_connected_A))
+    # Save the post-simulation adjacency matrix.
+    push!(post_networks, (; fw_ID = string(fwid), model = string(model_name), post_adj = out.post_adj);
+                          promote = true)
 
 end
 
+
 # --- 5. Save Simulation Summary ---
 mkpath(joinpath(@__DIR__, "data", "outputs"))
-CSV.write(joinpath(@__DIR__, "data", "outputs", "END_simulation_summary_03_05_2026.csv"), simulation_summary)
 
-JLD2.save_object(joinpath(@__DIR__, "data", "outputs", "END_final_adj_03_05_2026.jld2"), final_network)
+CSV.write(joinpath(@__DIR__, "data", "outputs", "dynamic_metrics.csv"), dynamic_metrics)
+
+JLD2.save_object(joinpath(@__DIR__, "data", "outputs", "post_networks.jld2"), post_networks)
